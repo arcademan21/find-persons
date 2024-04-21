64c34095628ff7bbe8167182ae4be200f6e0eea0
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-04-21 17:18:18 +0200

    Subiendo cambios

--- src/components/TefpayPaymentForm.jsx
+++ src/components/TefpayPaymentForm.jsx
@@ -199,8 +199,8 @@ export const TefpayPaymentForm = () => {
             <input type="hidden" name="Ds_Merchant_Subscription_PeriodInterval" value="1"/>
             <input type="hidden" name="Ds_Merchant_Subscription_Iteration" value="0"/>
             <input type="hidden" name="Ds_Merchant_Url" value={ tefpay_notyfi_url } />
-            <input type="hidden" name="Ds_Merchant_UrlOK" value={ `https://${hostname}${extension}/thanks/${paymentToken}` } />
-            <input type="hidden" name="Ds_Merchant_UrlKO" value={ `https://${hostname}${extension}/tefpay_error/error` } />
+            <input type="hidden" name="Ds_Merchant_UrlOK" value={ `https://${hostname}${(extension !== '/' ? extension: null )}/thanks/${paymentToken}` } />
+            <input type="hidden" name="Ds_Merchant_UrlKO" value={ `https://${hostname}${(extension !== '/' ? extension: null )}/tefpay_error/error` } />
             <input type="hidden" name="Ds_Merchant_MerchantCode" value="V99000566" />
             <input type="hidden" name="Ds_Merchant_MerchantCodeTemplate" value="V99000566" />
             <input type="hidden" name="Ds_Merchant_TemplateNumber" value="07" />
