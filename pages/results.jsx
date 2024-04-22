43b80ce7cb4bd076b45f8e487e6e87be11133ba7
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-04-22 22:04:24 +0200

    subiendo cambios

--- pages/results.jsx
+++ pages/results.jsx
@@ -250,6 +250,8 @@ const Results = () => {
     useEffect(() => {
         GetSuscription( user ).then( suscripted => {
             console.log('Suscripted: ', suscripted)
+            console.log('User: ', user)
+            console.log('Search: ', search)
             if( user && !suscripted && !search ) window.location.replace('/')
             else if( !user ) window.location.replace('/register')
 
