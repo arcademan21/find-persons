f8e607a7c09f73793d02aedf4af23ec7ba7e9e61
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-04-22 20:45:07 +0200

    Subiendo cambios

--- pages/payment.jsx
+++ pages/payment.jsx
@@ -1,5 +1,5 @@
 'use client'
-import {useEffect, useContext, useState} from "react"
+import {useEffect, useContext, useState, useLayoutEffect} from "react"
 import TefpayPaymentForm from "@/components/TefpayPaymentForm"
 import GlobalContext from "@/context/GlobalContext"
 import VantaGlobe from '@/components/VantaGlobe'
