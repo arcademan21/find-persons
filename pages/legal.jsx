5e862e595a842aef8902d07cdaccb91b838e3082
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-04-23 16:04:44 +0200

    subiendo cambios

---
+++ pages/legal.jsx
@@ -0,0 +1,183 @@
+'use client'
+import Head from 'next/head';
+
+const Legal = () => {
+
+    return (<>
+
+        <Head>
+            <meta name="robots" content="noindex" />
+        </Head>
+
+        <div className="container py-5">
+            
+            <div className="page-banner my-5">
+                <div className="row justify-content-center align-items-center h-100">
+                    <div className="col-md-6">
+                    <h1 className="text-center">Aviso legal</h1>
+                    </div>
+                </div>
+            </div>
+
+            <main>
+                <div class="page-section">
+                    <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-lg-12 py-3">
+                        
+                        <h2 class="title-section">Identificación y Titularidad</h2>
+                        <div class="divider"></div>
+                        <p>
+                            En cumplimiento del artículo 10 de la Ley 34/2002,
+                            de 11 de julio, de Servicios de la Sociedad de la Información y Comercio Electrónico,
+                            el Titular expone sus datos identificativos.
+                        </p>
+
+                        <ul>
+                            <li> Titular: MONTAUP PUBLICISTES S.L.U.</li>
+                            <li> Nombre comercial: ADS DIGITAL</li>
+                            <li> NRT: L-714323-H</li>
+                            <li> NUM. REGISTRO SOCIEDAD: 19221</li>
+                            <li> Sitio Web: www.find-persons.com</li>
+                            <li> Correo electrónico: support@find-persons.com</li>
+                            <li> 
+                            Domicilio: Carrer Esteve Albert, R. la Roureda, bloc A, 1-2a
+                            AD500 Santa Coloma, Andorra La Vella, PRINCIPAT D’ANDORRA
+                            </li>
+                        </ul>
+
+                        <h2 class="title-section">Finalidad</h2>
+                        <div class="divider"></div>
+
+                        <p>
+                            La finalidad del sitio Web <a href="https://find-persons.com">find-persons.com</a> es realizar consultas a
+                            distintas bases de datos a la vez con el fin de obtener el máximo de datos
+                            relevantes sobre una persona o individuo.
+                        </p>  
+
+                        <h2 class="title-section">Condiciones de Uso</h2>
+                        <div class="divider"></div>
+
+                        <p>
+                            La utilización del sitio Web te otorga la condición de Usuario, e implica la aceptación
+                            completa de todas las cláusulas y condiciones de uso incluidas en las páginas:
+                        </p>  
+
+                        <ul>
+                            <li><a href="https://find-persons.com/terminos-condiciones">Términos y condiciones</a></li>
+                            <li><a href="https://find-persons.com/politicas-privacidad">Política de Privacidad</a></li>
+                            <li><a href="https://find-persons.com/politicas-cookies">Política de Cookies</a></li>
+                        </ul> 
+
+                        <p>
+                            Si no estás conforme con todas y cada una de estas cláusulas y condiciones te
+                            abstendrás de utilizar este sitio Web.
+                        </p>
+
+                        <p>
+                            El acceso a este sitio Web no supone, en modo alguno, el inicio de una relación
+                            comercial con Ads Digital.
+                        </p>
+
+                        <p>
+                            A través de este sitio Web, el Titular te facilita el acceso y la utilización de diversos
+                            contenidos que el Titular o sus colaboradores han publicado por medio de Internet.
+                        </p>
+
+                        <p>
+                            A tal efecto, te obligas y comprometes a NO utilizar cualquiera de los contenidos del
+                            sitio Web con fines o efectos ilícitos, prohibidos en este Aviso Legal o por la
+                            legislación vigente, lesivos de los derechos e intereses de terceros, o que de
+                            cualquier forma puedan dañar, inutilizar, sobrecargar, deteriorar o impedir la normal
+                            utilización de los contenidos, los equipos informáticos o los documentos, archivos y
+                            toda clase de contenidos almacenados en cualquier equipo informático propios o
+                            contratados por Ads Digital, de otros usuarios o de cualquier usuario de Internet.
+                        </p>
+
+                        <h2 class="title-section">Medidas de seguridad</h2>
+                        <div class="divider"></div>
+
+                        <p>
+                            Los datos personales que facilites al Titular pueden ser almacenados en bases de
+                            datos automatizadas o no, cuya titularidad corresponde en exclusiva a <!--Ads Digital --><img src="/wp-public/theme_search_persons/assets/img/ads.png"/>,
+                            que asume todas las medidas de índole técnica, organizativa y de seguridad que
+                            garantizan la confidencialidad, integridad y calidad de la información contenida en
+                            las mismas de acuerdo con lo establecido en la normativa vigente en protección de
+                            datos.
+                        </p>  
+
+                        <p>
+                            No obstante, debes ser consciente de que las medidas de seguridad de los sistemas
+                            informáticos en Internet no son enteramente fiables y que, por tanto el Titular no
+                            puede garantizar la inexistencia de virus u otros elementos que puedan producir
+                            alteraciones en los sistemas informáticos (software y hardware) del Usuario o en sus
+                            documentos electrónicos y ficheros contenidos en los mismos aunque el Titular pone
+                            todos los medios necesarios y toma las medidas de seguridad oportunas para evitar
+                            la presencia de estos elementos dañinos.
+                        </p>
+
+                        <h2 class="title-section">Tratamiento de Datos Personales</h2>
+                        <div class="divider"></div>
+
+                        <p>
+                            Puedes consultar toda la información relativa al tratamiento de datos personales que
+                            recoge el Titular en la página de <a href="https://find-persons.com/politicas-privacidad">Política de Privacidad.</a>
+                        </p>  
+
+                        <h2 class="title-section">Contenidos</h2>
+                        <div class="divider"></div>
+
+                        <p>
+                            El Titular ha obtenido la información, el contenido multimedia y los materiales
+                            incluidos en el sitio Web de fuentes que considera fiables, pero, si bien ha tomado
+                            todas las medidas razonables para asegurar que la información contenida es
+                            correcta, el Titular no garantiza que sea exacta, completa o actualizada. Ads Digital
+                            declina expresamente cualquier responsabilidad por error u omisión en la
+                            información contenida en las páginas de este sitio Web.
+                        </p>  
+
+                        <p>
+                            Queda prohibido transmitir o enviar a través del sitio Web cualquier contenido ilegal
+                            o ilícito, virus informáticos, o mensajes que, en general, afecten o violen derechos
+                            de el Titular o de terceros.
+                        </p>
+
+                        <p>
+                            Los contenidos de <a href="https://find-persons.com">find-persons.com</a> tienen únicamente una finalidad
+                            informativa y bajo ninguna circunstancia deben usarse ni considerarse como oferta
+                        </p>  
+
+                        <p>
+                            de venta, solicitud de una oferta de compra ni recomendación para realizar cualquier
+                            otra operación, salvo que así se indique expresamente.
+                        </p>
+
+                        <p>
+                            Ads Digital se reserva el derecho a modificar, suspender, cancelar o restringir el
+                            contenido de <a href="https://find-persons.com">find-persons.com</a>, los vínculos o la información obtenida a
+                            través del sitio Web, sin necesidad de previo aviso.
+                        </p>
+
+                        <p>
+                            Ads Digital no es responsable de los daños y perjuicios que pudieran derivarse de la
+                            utilización de la información del sitio Web.
+                        </p>
+
+
+                        
+
+                        
+                        </div>
+                        
+                    </div>
+                    </div> 
+                </div> 
+            </main>
+
+
+        </div>
+    </>)
+
+}
+
+export default Legal
