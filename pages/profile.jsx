5834d3db533d2dfb2dbb955e84b01e47e2949511
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-04-23 10:55:19 +0200

    subiendo cambios

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -87,8 +87,14 @@ const Profile = () => {
 
     }, [])
 
+    
+
 
     return ( <>
+
+
+
+
 
         <div className="profile-container">
             <div className="container py-5 vh-100">
@@ -96,53 +102,65 @@ const Profile = () => {
                     
                     {/* USER INFO */}
                     <div className="col-md-6">
+
+
+                        { userData && !userData.user_data ?
+                            <div className="alert alert-danger">
+                                Aun no tienes una cuenta, por favor registrate.
+                                <a href="/register">Registrarse gratis</a>
+                            </div>
+                            :
                         
-                        <h2>Mi cuenta</h2>
-                        <div className="card profile-data mb-3 p-3">
-                            <p className="mb-1">
-                                <strong>Nombre : </strong> 
-                                { userData && userData.user_data.user_name }
-                            </p>
-                            <p className="mb-1">
-                                <strong>Correo : </strong> 
-                                { userData && userData.user_data.user_email }
-                            </p>
-                        </div>
+                            <>
+                            <h2>Mi cuenta</h2>
+                            <div className="card profile-data mb-3 p-3">
+                                <p className="mb-1">
+                                    <strong>Nombre : </strong> 
+                                    { userData && userData.user_data.user_name }
+                                </p>
+                                <p className="mb-1">
+                                    <strong>Correo : </strong> 
+                                    { userData && userData.user_data.user_email }
+                                </p>
+                            </div>
 
-                        <h2>Suscription</h2>
-                        <div className="card profile-data mb-3 p-3">
-                            <p className="mb-1">
-                                <strong> Estado de la suscripcion : </strong> 
-                                { userData && userData.suscription_data.status }
-                            </p>
-                            <p className="mb-1">
-                                <strong>Fin de prueba : </strong> 
-                                { userData && userData.suscription_data.end_trial }
-                            </p>
+                            <h2>Suscription</h2>
+                            <div className="card profile-data mb-3 p-3">
+                                <p className="mb-1">
+                                    <strong> Estado de la suscripcion : </strong> 
+                                    { userData && userData.suscription_data.status }
+                                </p>
+                                <p className="mb-1">
+                                    <strong>Fin de prueba : </strong> 
+                                    { userData && userData.suscription_data.end_trial }
+                                </p>
 
-                            { userData && userData.suscription_data.status !== 'canceled' ?
-                                <>
-                                    <p>
-                                        Recuerda que si no deseas continuar con la suscripcion, puedes darte de baja en cualquier momento.
-                                        Si tienes alguna duda, puedes contactar con nosotros en { process.env.NEXT_PUBLIC_CONTACT_EMAIL }, envianos un correo y te responderemos lo antes posible.
-                                    </p>
-                                    <button className="btn btn-secondary"
-                                        onClick={ async ()=>{
-                                            const res = await downSuscription( userData.user_data.user_email, userData.suscription_data.payment_id )
-                                            if( !res ) console.log('Error al darse de baja : ', res)
-                                            else {
-                                                console.log('Dado de baja correctamente : ', res)
-                                                //window.location.replace( '/' )
-                                            }
-                                        } }
-                                    > Darse de baja </button>
-                                </>
-                                :
-                                null
+                                { userData && userData.suscription_data.status !== 'canceled' ?
+                                    <>
+                                        <p>
+                                            Recuerda que si no deseas continuar con la suscripcion, puedes darte de baja en cualquier momento.
+                                            Si tienes alguna duda, puedes contactar con nosotros en { process.env.NEXT_PUBLIC_CONTACT_EMAIL }, envianos un correo y te responderemos lo antes posible.
+                                        </p>
+                                        <button className="btn btn-secondary"
+                                            onClick={ async ()=>{
+                                                const res = await downSuscription( userData.user_data.user_email, userData.suscription_data.payment_id )
+                                                if( !res ) console.log('Error al darse de baja : ', res)
+                                                else {
+                                                    console.log('Dado de baja correctamente : ', res)
+                                                    //window.location.replace( '/' )
+                                                }
+                                            } }
+                                        > Darse de baja </button>
+                                    </>
+                                    :
+                                    null
 
-                            }
+                                }
 
-                        </div>
+                            </div>
+                            </>
+
+                        }
 
                     </div>
 
