9e401af613ad6d1e4c27142c65376ed9dcf16c8a
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-04-23 12:31:02 +0200

    subiendo cambios

--- pages/it/_app.jsx
+++ pages/it/_app.jsx
@@ -47,23 +47,18 @@ function PersonsFinder({ Component, pageProps }) {
     <Script type="text/javascript" src="/libs/jquery/jquery.min.js" />
     <Script type="text/javascript" src="/libs/bootstrap/js/bootstrap.min.js" />
     <Script type="text/javascript" src="/libs/bootstrap/js/bootstrap.bundle.min.js" />
-    {/* <Script type="text/javascript" src="/libs/owl-carousel/owl.carousel.min.js" strategy='lazyOnload' />
-    <Script type="text/javascript" src="/libs/waypoints/jquery.waypoints.min.js" strategy='lazyOnload' />
-    <Script type="text/javascript" src="/libs/animateNumber/jquery.animateNumber.min.js"  strategy='lazyOnload' /> */}
+    
     <Script type="text/javascript" src="/libs/font-awesome/js/all.min.js"  />
     <Script type="text/javascript" src="/libs/vantajs/three.min.js" strategy='beforeInteractive' />
     <Script type="text/javascript" src="/libs/vantajs/vanta.globe.min.js" strategy='beforeInteractive' /> 
-    <Script type="text/javascript" src="/libs/wow/wow.min.js" />
-    <Script type="text/javascript" src="/libs/wow/wow.init.js" />
-
     
     <Script type="text/javascript" src="/js/theme.js" strategy='lazyOnload' />
     <Script type="text/javascript" src="/libs/wow/wow.min.js" strategy="beforeInteractive" />
     <Script type="text/javascript" src="/libs/wow/wow.init.js" strategy="afterInteractive" />
 
-
-    
-
+    <Script type="text/javascript" src="/libs/owl-carousel/owl.carousel.min.js" />
+    <Script type="text/javascript" src="/libs/waypoints/jquery.waypoints.min.js" />
+    <Script type="text/javascript" src="/libs/animateNumber/jquery.animateNumber.min.js"  />
 
 
     
