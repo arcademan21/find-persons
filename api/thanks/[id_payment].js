d42233fc9158bf394bde32b5bc197eb4480aaa0c
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-04-30 15:49:44 +0200

    Subiendo cambios

--- api/thanks/[id_payment].js
+++ api/thanks/[id_payment].js
@@ -1,10 +1,15 @@
 
-module.exports = (req, res) => {
+
+export default (req, res) => {
+    
+    const { query: { id_payment } } = req;
+
     if (req.method === 'POST') {
         // Procesa los datos POST aquí
-        res.status(200).send({ message: 'Pago procesado correctamente' });
+        res.status(200).json({ message: 'Pago procesado correctamente', id: id_payment });
     } else {
         // Método no permitido
         res.status(405).send({ error: 'Método no permitido' });
     }
+
 };
