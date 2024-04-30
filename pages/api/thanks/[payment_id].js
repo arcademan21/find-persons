7fa2c0091a1dd2d83e9c5f62957bd9fad75a093f
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-04-30 16:39:55 +0200

    Subiendo cambios

--- pages/api/thanks/[payment_id].js
+++ pages/api/thanks/[payment_id].js
@@ -4,9 +4,7 @@ export default function handler(req, res) {
     
     if (req.method === 'POST') {
         // Procesa los datos POST aquí
-        res.status(200).json({ 
-            message: 'Pago procesado correctamente', paymentId: req.query.payment_id 
-        });
+        
         // Rediriges al usuario a la página de agradecimiento con el ID del pago
         res.redirect(`/thanks/${req.query.payment_id}`);
     } else {
