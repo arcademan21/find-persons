5aaa3a0386ca88052971085649723cec051af63b
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-02 11:54:07 +0200

    Actualizando frontend

--- pages/api/thanks/[payment_id].js
+++ pages/api/thanks/[payment_id].js
@@ -1,170 +1,170 @@
 
 
-const path_endpoint = process.env.NEXT_PUBLIC_PATH_END_POINT
+// const path_endpoint = process.env.NEXT_PUBLIC_PATH_END_POINT
 
-const CheckTokenValidity = async ( token ) => {
+// const CheckTokenValidity = async ( token ) => {
     
-    try{
+//     try{
 
-        // Fetch to endpoint for get payment
-        const req = await fetch( path_endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                "petition" : {
-                    "name": "validate_payment_token",
-                    "data": {
-                        "validate_payment_token": {
-                            "payment_id": token
-                        }
-                    }
-                }
-            })
-        })
+//         // Fetch to endpoint for get payment
+//         const req = await fetch( path_endpoint, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({
+//                 "petition" : {
+//                     "name": "validate_payment_token",
+//                     "data": {
+//                         "validate_payment_token": {
+//                             "payment_id": token
+//                         }
+//                     }
+//                 }
+//             })
+//         })
         
-        const res = await req.json()
-        if( res.status === 'error' ) return false
+//         const res = await req.json()
+//         if( res.status === 'error' ) return false
 
-    } catch ( error ) {
-        return false
-    }
+//     } catch ( error ) {
+//         return false
+//     }
     
-    return true
+//     return true
     
-}
+// }
 
-const InvalidateToken = async ( token ) => {
+// const InvalidateToken = async ( token ) => {
     
-    try{
+//     try{
 
-        // Fetch to endpoint for get payment
-        const req = await fetch( path_endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                "petition" : {
-                    "name": "invalidate_payment_token",
-                    "data": {
-                        "invalidate_payment_token": {
-                            "payment_id": token
-                        }
-                    }
-                }
-            })
-        })
+//         // Fetch to endpoint for get payment
+//         const req = await fetch( path_endpoint, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({
+//                 "petition" : {
+//                     "name": "invalidate_payment_token",
+//                     "data": {
+//                         "invalidate_payment_token": {
+//                             "payment_id": token
+//                         }
+//                     }
+//                 }
+//             })
+//         })
         
-        const res = await req.json()
-        if( res.status === 'error' ) return false
+//         const res = await req.json()
+//         if( res.status === 'error' ) return false
 
-    } catch ( error ) {
-        return false
-    }
+//     } catch ( error ) {
+//         return false
+//     }
     
-    return true
+//     return true
 
 
-}
+// }
 
-const ExistsPayment = async ( payment_id ) => {
+// const ExistsPayment = async ( payment_id ) => {
     
-    try{
+//     try{
 
-        // Fetch to endpoint for get payment
-        const req = await fetch( path_endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                "petition" : {
-                    "name": "exists_payment",
-                    "data": {
-                        "payment": {
-                            "payment_id": payment_id
-                        }
-                    }
-                }
-            })
-        })
+//         // Fetch to endpoint for get payment
+//         const req = await fetch( path_endpoint, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({
+//                 "petition" : {
+//                     "name": "exists_payment",
+//                     "data": {
+//                         "payment": {
+//                             "payment_id": payment_id
+//                         }
+//                     }
+//                 }
+//             })
+//         })
         
-        const res = await req.json()
-        if( res.status === 'error' ) return false
+//         const res = await req.json()
+//         if( res.status === 'error' ) return false
 
-    } catch ( error ) {
-        return false
-    }
+//     } catch ( error ) {
+//         return false
+//     }
     
-    return true
-}
+//     return true
+// }
 
-const CreateNewUser = async ( user ) => {
+// const CreateNewUser = async ( user ) => {
 
-    const country = localStorage.getItem('language')
+//     const country = localStorage.getItem('language')
   
-    try{
+//     try{
         
-        // Fetch to endpoint for get payment
-        const req = await fetch( path_endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                "petition" : {
-                    "name": "create_new_user",
-                    "data": {
-                        "create_new_user": {
-                            "user_name": user.displayName ? user.displayName : user.email,
-                            "user_email": user.email,
-                            "password": user.uid,
-                            "role": "suscriber",
-                            "status": "active",
-                            "country": country,
-                            "ip": ""
-                        }
-                    }
-                }
-            })
-        })
+//         // Fetch to endpoint for get payment
+//         const req = await fetch( path_endpoint, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({
+//                 "petition" : {
+//                     "name": "create_new_user",
+//                     "data": {
+//                         "create_new_user": {
+//                             "user_name": user.displayName ? user.displayName : user.email,
+//                             "user_email": user.email,
+//                             "password": user.uid,
+//                             "role": "suscriber",
+//                             "status": "active",
+//                             "country": country,
+//                             "ip": ""
+//                         }
+//                     }
+//                 }
+//             })
+//         })
         
-        const res = await req.json()
-        if( res.status === 'error' ) return false
+//         const res = await req.json()
+//         if( res.status === 'error' ) return false
             
-    } catch ( error ) {
-        return false
-    }
+//     } catch ( error ) {
+//         return false
+//     }
 
-    return true
-}
+//     return true
+// }
 
-const UpdateSuscription = async ( user, suscription ) => {
+// const UpdateSuscription = async ( user, suscription ) => {
 
-    try{
+//     try{
 
-        // Fetch to endpoint for update suscription
-        const req = await fetch( path_endpoint, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                "petition" : {
-                    "name": "update_suscription",
-                    "data": {
-                        "update_suscription": {
-                            "user_email": user.email,
-                            "payment_id": suscription.payment_id,
-                            "status": "trial",
-                        }
-                    }
-                }
-            })
-        })
+//         // Fetch to endpoint for update suscription
+//         const req = await fetch( path_endpoint, {
+//             method: 'POST',
+//             headers: { 'Content-Type': 'application/json' },
+//             body: JSON.stringify({
+//                 "petition" : {
+//                     "name": "update_suscription",
+//                     "data": {
+//                         "update_suscription": {
+//                             "user_email": user.email,
+//                             "payment_id": suscription.payment_id,
+//                             "status": "trial",
+//                         }
+//                     }
+//                 }
+//             })
+//         })
 
-        const res = await req.json()
-        if( res.status === 'error' ) return false
+//         const res = await req.json()
+//         if( res.status === 'error' ) return false
 
-    } catch ( error ) {
-        return false
-    }
+//     } catch ( error ) {
+//         return false
+//     }
 
-    return true
+//     return true
 
-}
+// }
 
 export default function handler( req, res ) {
     
