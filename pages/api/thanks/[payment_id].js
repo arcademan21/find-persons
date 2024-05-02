e28208e72ca48851ede4ae2d636dc5f6b4805f21
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-02 09:55:56 +0200

    Subiendo cambios

--- pages/api/thanks/[payment_id].js
+++ pages/api/thanks/[payment_id].js
@@ -1,19 +1,25 @@
 
 
-export default function handler(req, res) {
+export default function handler( req, res ) {
     
-    // if (req.method === 'POST') {
-    //     // Procesa los datos POST aquí
+    const method = req.method
+    const extension  = req.query.payment_id
+
+    if ( req.method === 'POST' ) 
+        res.json({ 
+            message: 'POST request to the API',
+            method: method,
+            payment_id: extension.split('-')[1] 
+        })
+        //res.redirect( 303, `/thanks/${req.query.payment_id} `)
         
-    //     // Rediriges al usuario a la página de agradecimiento con el ID del pago
         
     // } else {
     //     // Método no permitido
-    //     res.status(405).json({ error: 'Método no permitido' });
+    //     res.status(405).json({ error: 'Método no permitido' })
     // }
 
 
-    res.redirect(303, `/thanks/${req.query.payment_id}`);
 
 
 
