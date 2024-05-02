b83834c1305d972c835bf98041597704378e9df2
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-02 15:17:10 +0200

    Actualizando frontend

--- pages/api/thanks/[payment_id].js
+++ pages/api/thanks/[payment_id].js
@@ -122,7 +122,8 @@ export const CreateNewUser = async ( user ) => {
         })
         
         const res = await req.json()
-        if( res.status === 'error' ) return false
+        return res
+        //if( res.status === 'error' ) return false
             
     } catch ( error ) {
         return false
