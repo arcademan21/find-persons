c35700ab7f6b23c099a10c4ada28393b68f358b1
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-04 20:47:58 +0200

    Actualizando frontend

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,7 @@ import Header from "../src/components/Header"
 import Footer from "../src/components/Footer"
 import Script from 'next/script'
 import Head from 'next/head'
+import CookieConsent, { Cookies } from "react-cookie-consent"
 
 export const metadata = {
   title: 'Persons - Finder',
@@ -38,6 +39,17 @@ function PersonsFinder({ Component, pageProps }) {
     />
     
     <GlobalProvider>
+        <CookieConsent
+            location="bottom"
+            buttonText="Sure man!!"
+            cookieName="myAwesomeCookieName2"
+            style={{ background: "#2B373B" }}
+            buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+            expires={150}
+        >
+            This website uses cookies to enhance the user experience.{" "}
+            <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
+        </CookieConsent>
         <Header />
         <Component { ...pageProps } />
         <Footer />
