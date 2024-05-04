993b2d2440008f8dcb19179d9935afa13747eede
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-04 21:14:40 +0200

    Actualizando frontend

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import Loader from "../components/Loader"
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Script from 'next/script'
-import CookieConsent from "react-cookie-consent"
+
 
 export const metadata = {
   title: 'Persons - Finder',
@@ -60,21 +60,6 @@ function RootLayout( { children } ) {
                       </div>
                   </div>
                 </div>
-
-
-                <CookieConsent
-                    location="bottom"
-                    buttonText="Sure man!!"
-                    cookieName="myAwesomeCookieName2"
-                    style={{ background: "#2B373B" }}
-                    buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-                    expires={150}
-                >
-                    This website uses cookies to enhance the user experience.{" "}
-                    <span style={{ fontSize: "10px" }}>This bit of text is smaller :O</span>
-                </CookieConsent>
-
-
               
                 <Footer />
           </GlobalProvider>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,25 @@
 import Hero from '@/components/Hero'
 import HomeSections from '@/components/HomeSections'
-import styles from "./css/page.module.css"
+import CookieConsent from "react-cookie-consent"
 
 const Home = () => {
 
     return (<>
         <Hero />
         <HomeSections />
+        <CookieConsent
+            location="bottom"
+            buttonText="Sure man!!"
+            cookieName="myAwesomeCookieName2"
+            style={{ background: "#2B373B" }}
+            buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+            expires={150}
+        >
+            This website uses cookies to enhance the user experience.{" "}
+            <span style={{ fontSize: "10px" }}>
+                This bit of text is smaller :O
+            </span>
+        </CookieConsent>
     </>)
 
 }
