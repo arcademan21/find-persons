474ac3c70717f5a18d30e0e61a161e9a4eade1ec
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-04 21:33:21 +0200

    Actualizando frontend

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,6 +5,8 @@ import Header from "../src/components/Header"
 import Footer from "../src/components/Footer"
 import Script from 'next/script'
 import Head from 'next/head'
+import CookieConsent from "react-cookie-consent"
+
 
 export const metadata = {
   title: 'Persons - Finder',
@@ -39,10 +41,28 @@ function PersonsFinder({ Component, pageProps }) {
     
     <GlobalProvider>
         
+        
+
+
         <Header />
         <Component { ...pageProps } />
         <Footer />
+
     </GlobalProvider>
+
+    <CookieConsent
+        location="bottom"
+        buttonText="Sure man!!"
+        cookieName="myAwesomeCookieName2"
+        style={{ background: "#2B373B" }}
+        buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+        expires={150}
+    >
+        This website uses cookies to enhance the user experience.{" "}
+        <span style={{ fontSize: "10px" }}>
+            This bit of text is smaller :O
+        </span>
+    </CookieConsent>
     
     <Script type="text/javascript" src="/libs/jquery/jquery.min.js" strategy='beforeInteractive'/>
     <Script type="text/javascript" src="/libs/bootstrap/js/bootstrap.min.js" strategy='beforeInteractive'/>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,11 @@
 import Hero from '@/components/Hero'
 import HomeSections from '@/components/HomeSections'
 
-import CookieConsent from "react-cookie-consent"
-
-
-
 const Home = () => {
 
     return (<>
         <Hero />
         <HomeSections />
-        <CookieConsent
-            location="bottom"
-            buttonText="Sure man!!"
-            cookieName="myAwesomeCookieName2"
-            style={{ background: "#2B373B" }}
-            buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
-            expires={150}
-        >
-            This website uses cookies to enhance the user experience.{" "}
-            <span style={{ fontSize: "10px" }}>
-                This bit of text is smaller :O
-            </span>
-        </CookieConsent>
-        
     </>)
 
 }
