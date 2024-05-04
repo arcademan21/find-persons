2fc42a2ac5a010e344093d202bb6d26852bc3819
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-04 23:09:49 +0200

    Actualizando frontend

---
+++ pages/ae/profile.jsx
@@ -0,0 +1,196 @@
+'use client'
+import { useEffect, useState } from "react"
+
+const Profile = () => {
+
+    const [userData, setUserData] = useState( null )
+    const path_endpoint = process.env.NEXT_PUBLIC_PATH_END_POINT
+    const language = JSON.parse(localStorage.getItem('language_file')).profile
+    const laguage_toast = JSON.parse(localStorage.getItem('language_file')).toast
+    const extension = localStorage.getItem('extencion')
+
+    const getUserData = async ( email ) => {
+
+        try {
+            
+            const req = await fetch( path_endpoint, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    "petition" : {
+                        "name": "get_user_data",
+                        "data": {
+                            "get_user_data": {
+                                "user_email": email
+                            }
+                        }
+                    }
+                })
+            })
+
+            const res = await req.json()
+            if( res.status === 'error' ) return false
+            
+            return res
+
+        }
+        catch ( error ) {
+            return false
+        }
+    }
+
+    const downSuscription = async ( email, payment_id ) => {
+
+        if( confirm('¿Estas seguro de que deseas darte de baja?') === false ) return false
+
+        try {
+
+            const req = await fetch( path_endpoint, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    "petition" : {
+                        "name": "down_suscription",
+                        "data": {
+                            "down_suscription": {
+                                "user_email": email,
+                                "payment_id": payment_id,
+                                "reason": "user request"
+                            }
+                        }
+                    }
+                })
+            })
+
+            const res = await req.json()
+            if( res.status === 'error' ) return false
+            return true
+
+        }
+        catch ( error ) {
+            return false
+        }
+
+    }
+
+    useEffect(() => {
+        
+        const user = JSON.parse(localStorage.getItem('user'))
+        if( !user ) {
+            window.location.replace(extension)
+        }
+
+        getUserData( user.email ).then( res => { 
+            
+            setUserData( res )
+
+        })
+
+        
+
+    }, [])
+
+    return ( <>
+
+        <div className="profile-container">
+            <div className="container py-5 vh-100">
+                <div className="row py-5 justify-content-center">
+                    
+                    {/* USER INFO */}
+                    <div className="col-md-6">
+
+                        { userData && !userData.user_data ?
+                            <div className="alert alert-danger">
+                                { language.no_suscripted_message }
+                                <a href={`${extension}/payment`}>  
+                                    { language.suscribe }
+                                </a>
+                            </div>
+                            :
+                            <>
+                            <h2>
+                                { language.my_account_title }
+                            </h2>
+                            <div className="card profile-data mb-3 p-3">
+                                <p className="mb-1">
+                                    <strong>
+                                        { language.name } :
+                                    </strong> 
+                                    { userData && userData.user_data.user_name }
+                                </p>
+                                <p className="mb-1">
+                                    <strong> 
+                                    { language.email }
+                                    : </strong> 
+                                    { userData && userData.user_data.user_email }
+                                </p>
+                            </div>
+
+                            <h2>
+                                { language.suscription_title }
+                            </h2>
+                            <div className="card profile-data mb-3 p-3">
+                                <p className="mb-1">
+                                    <strong> 
+                                    { language.status }
+                                    : </strong> 
+                                    { userData && userData.suscription_data.status }
+                                </p>
+                                <p className="mb-1">
+                                    <strong>
+                                        { language.end_trial}
+                                    : </strong> 
+                                    { userData && userData.suscription_data.end_trial }
+                                </p>
+
+                                { userData && userData.suscription_data.status !== 'canceled' ?
+                                    <>
+                                        <p>
+                                            { language.paragraph_1 }{ process.env.NEXT_PUBLIC_CONTACT_EMAIL }{ language.paragraph_1b}
+                                        </p>
+                                        <button className="btn btn-secondary"
+                                            onClick={ async ()=>{
+                                                const res = await downSuscription( userData.user_data.user_email, userData.suscription_data.payment_id )
+                                                if( !res ) {
+                                                    toast.error(language_toast.error_down_suscription_message)
+                                                }
+                                                else {
+                                                    
+                                                    let success_message = async () => {
+                                                        toast.success(language_toast.success_down_suscription_message)
+                                                    }
+
+                                                    success_message().then( () => {
+                                                        window.location.replace( '/' )
+                                                    })
+
+                                                }
+                                            } }
+                                        >  
+                                            { language.down_suscription}
+                                        </button>
+                                    </>
+                                    :
+                                    null
+
+                                }
+
+                            </div>
+                            </>
+
+                        }
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </>)
+
+
+
+
+}
+
+export default Profile
---
+++ pages/ae/search.jsx
@@ -0,0 +1,274 @@
+'use client'
+import { useContext, useState, useEffect } from "react"
+import GlobalContext from '@/context/GlobalContext'
+import Image from 'next/image'
+import '../css/search.css'
+import { FaSpinner, FaCheck, FaUser, FaMapMarkedAlt, FaUsers } from 'react-icons/fa'
+
+const path_endpoint = process.env.NEXT_PUBLIC_PATH_END_POINT
+const GetSuscription = async ( user ) =>{
+    
+    try{
+
+        // Fetch to endpoint for update suscription
+        const req = await fetch( path_endpoint, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "petition" : {
+                    "name": "get_suscription",
+                    "data": {
+                        "get_suscription": {
+                            "user_email": user.email,
+                        }
+                    }
+                }
+            })
+        })
+
+        const res = await req.json()
+        if( res.status === 'error' ) return false
+        return res
+
+    } catch ( error ) {
+        return false
+    }
+
+}
+
+const Search = () => {  
+    
+    const context = useContext( GlobalContext )
+    const { state, setState } = context
+
+    const search = localStorage.getItem('search')
+    const language = JSON.parse( localStorage.getItem('language_file') ).search_page
+    const user = JSON.parse( localStorage.getItem('user') )
+    
+    const [text_search_info, setTextSearchInfo] = useState('')
+    const [awaitText, setAwaitText] = useState('')
+    const [activeClass, setActiveClass] = useState('')
+    const [stateIcon1, setStateIcon1] = useState(true)
+    const [stateIcon2, setStateIcon2] = useState(true)
+    const [stateIcon3, setStateIcon3] = useState(true)
+    const [stateIcon4, setStateIcon4] = useState(true)
+    const [stateIcon5, setStateIcon5] = useState(true)
+    const [stateIcon6, setStateIcon6] = useState(true)
+
+    const [listOfSearchs, setListOfSearchs] = useState([])
+    const extension = localStorage.getItem('extencion')
+
+    useEffect(() => {
+        if( !search ) window.location.replace(extension)
+        setListOfSearchs([
+            language.socials, 
+            language.work,
+            language.address, 
+            language.telephone, 
+            language.email, 
+            language.news, 
+            language.histories
+        ])
+
+    }, [])
+
+    useEffect(() => { 
+
+        let count = 0
+        let random = 0
+        let random_item = null
+
+        const time = setInterval(() => {
+
+            setTextSearchInfo(listOfSearchs[count])
+    
+            if ( count === 5 ) {
+                
+                clearInterval(time)
+                
+                setAwaitText('')
+                setActiveClass('active')
+                GetSuscription( user ).then( suscripted => {
+                    
+                    // Redirect 
+                    if( !user ) window.location.replace(`${extension}/register`)
+                    else if( !suscripted ) window.location.replace(`${extension}/payment`)
+                    else window.location.replace(`${extension}/results`)
+        
+                })
+                
+            }
+    
+            count++
+    
+            random = Math.floor( Math.random() * count ) + 0
+    
+            switch ( random ) {
+                case 0: setStateIcon1(false); break;
+                case 1: setStateIcon2(false); break;
+                case 2: setStateIcon3(false); break;
+                case 3: setStateIcon4(false); break;
+                case 4: setStateIcon5(false); break;
+                case 5: setStateIcon6(false); break;
+            }
+
+        }, 5000)
+         
+
+    }, [ listOfSearchs ] )
+
+    return (<>
+        
+        <div className="container py-5" >
+        
+            <div className="row content-search-map-anime my-3 mt-5 align-content-center" >
+                
+                <div className="col-sm-12 col-md-6 col-lg-6">
+                    <div className="animate_search">
+                        <Image src="/images/mapa_anime.gif" className="rounded " alt="" width={100} height={100} layout="responsive" />
+                    </div>
+                </div>
+
+                <div className="col-sm-12 col-md-6 col-lg-6" >
+                    <div className="percent-bar-content">
+                        
+                        <h3 className="title-section my-4 title-searching">
+                            <span data-section="search_page" data-value="we_searching">
+                                { language.we_searching }
+                            </span> 
+                            <span id="search" className="marked"> { search } </span>
+                            <span data-section="search_page" data-value="in_the" >
+                                { language.in_the }
+                            </span>
+                        </h3>
+
+                        <div className={`wrapper w-100 ${ activeClass }`}>
+
+                            { awaitText ? awaitText : 
+                                <div className="bg">
+                                    <div className="el"></div>
+                                </div>
+                            }
+                            
+                        </div>
+            
+                        <h1 className="text-center my-4 fs-3">
+                            <b id="text-search-info">{ text_search_info }</b>
+                        </h1>
+
+                    </div>
+                        
+                    <div className="content-checks-animate mb-3">
+                        
+                        <div className="container text-center">
+                            
+                            <ul className="checks-animate">
+                    
+                                <li className="item-check">
+                                    {stateIcon1 ? <FaSpinner className="mx-2 fa-spin" /> : <FaCheck className="mx-2" />}
+                                    <b data-section="search_page" data-value="socials">{listOfSearchs[0]}</b>
+                                </li>
+
+                                <li className="item-check">
+                                    {stateIcon2 ? <FaSpinner className="mx-2 fa-spin" /> : <FaCheck className="mx-2" />}
+                                    <b data-section="search_page" data-value="address">{listOfSearchs[1]}</b>
+                                </li>
+
+                                <li className="item-check">
+                                    {stateIcon3 ? <FaSpinner className="mx-2 fa-spin" /> : <FaCheck className="mx-2" />}
+                                    <b data-section="search_page" data-value="telephone">{listOfSearchs[2]}</b>
+                                </li>
+
+                                <li className="item-check">
+                                    {stateIcon4 ? <FaSpinner className="mx-2 fa-spin" /> : <FaCheck className="mx-2" />}
+                                    <b data-section="search_page" data-value="email">{listOfSearchs[3]}</b>
+                                </li>
+
+                                <li className="item-check">
+                                    {stateIcon5 ? <FaSpinner className="mx-2 fa-spin" /> : <FaCheck className="mx-2" />}
+                                    <b data-section="search_page" data-value="news">{listOfSearchs[4]}</b>
+                                </li>
+
+                                <li className="item-check">
+                                    {stateIcon6 ? <FaSpinner className="mx-2 fa-spin" /> : <FaCheck className="mx-2" />}
+                                    <b data-section="search_page" data-value="histories">{listOfSearchs[5]}</b>
+                                </li>
+
+                            </ul>
+
+                        </div>
+                    
+                    </div>
+        
+                </div>
+
+
+            </div>
+            <div className="divider w-100 m-0"></div>
+            <div className="row my-3">
+
+                <div className="col-md-6">
+        
+                    <div className="img-place text-center">
+                        <Image src="/images/bg_image_1_small.jpeg" className="mt-0" alt="" width={100} height={100} layout="responsive" />
+                    </div>
+        
+                </div>
+        
+                <div className="col-md-6">
+                    
+                    <div className="d-flex flex-column list-media py-5">
+                        <div className="d-flex align-items-start my-2">
+                            
+                            <FaUser className="mx-3" style={{fontSize: '6rem',
+                            color: '#6b53f2'}} />
+                            <div>
+                                <h6 className="mt-0 mb-1 fw-bold" data-section="search_page" data-value="title_accounts">
+                                    {language.title_accounts}
+                                </h6>
+                                <p className="text-muted" data-section="search_page" data-value="accounts_description">
+                                    {language.accounts_description}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="d-flex align-items-start my-2">
+                            
+                            <FaMapMarkedAlt className="mx-3" style={{fontSize: '6rem',
+                            color: '#6b53f2'}} />
+                            <div>
+                                <h6 className="mt-0 mb-1 fw-bold" data-value="title_location" data-section="search_page">
+                                    {language.title_location}
+                                </h6>
+                                <p className="text-muted" data-section="search_page" data-value="location_description">
+                                    {language.location_description}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="d-flex align-items-start my-2">
+                
+                            <FaUsers className="mx-3" style={{fontSize: '6rem',
+                            color: '#6b53f2'}} />
+                            <div>
+                                <h6 className="mt-0 mb-1 fw-bold" data-section="search_page" data-value="title_explore">
+                                    {language.title_explore}
+                                </h6>
+                                <p className="text-muted" data-section="search_page" data-value="explore_description">
+                                    {language.explore_description}
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    
+                   
+                    
+                </div>
+        
+            </div>
+
+        </div>
+        
+    </>)
+    
+}
+
+export default Search
