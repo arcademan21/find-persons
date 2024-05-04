8af329d12203ec84f153868f8473de360384e26c
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-04 21:19:35 +0200

    Actualizando frontend

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,6 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import CookieConsent from "react-cookie-consent"
+
 
 export const metadata = {
   title: 'Persons - Finder',
@@ -17,6 +19,19 @@ class MyDocument extends Document {
           <meta name="description" content={metadata.description} />
         </Head>
         <body>
+          <CookieConsent
+              location="bottom"
+              buttonText="Sure man!!"
+              cookieName="myAwesomeCookieName2"
+              style={{ background: "#2B373B" }}
+              buttonStyle={{ color: "#4e503b", fontSize: "13px" }}
+              expires={150}
+          >
+              This website uses cookies to enhance the user experience.{" "}
+              <span style={{ fontSize: "10px" }}>
+                  This bit of text is smaller :O
+              </span>
+          </CookieConsent>
           <Main />
           <NextScript />
         </body>
