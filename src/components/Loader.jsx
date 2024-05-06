83c5866b78d2c22ecae07f02d6cd40e794b8d689
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-06 19:02:27 +0200

    Subiendo cambios del front

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -11,35 +11,31 @@ const Loader = () => {
         
         setTimeout(() => {
             const loader = document.querySelector('.loader')
-            // if ( loader ) {
-            //     loader.classList.add('loader--fade')
-            //     loader.classList.add('d-none')
-            // }
-            //setLoading( false )
+            if ( loader ) {
+                loader.classList.add('loader--fade')
+                loader.classList.add('d-none')
+            }
+            setLoading( false )
         }, 1000 )
 
     }, [])
     
     return (
-        <div className="loader">
-            <div className="loader__logo text-center">
-                <Image src="/images/logo_find-persons.png" alt="Logo" width={100} height={100} 
-                    layout="responsive"
-                    style={{ width: '400px', height: '400px' }}
-                />
-            </div>
-        </div>
-        // <>
-        //     { loading ? 
-        //         <div className="loader">
-        //             <div className="loader__logo text-center">
-        //                 <Image src="/images/logo_find-persons.png" alt="Logo" width={100} height={100} 
-        //                     layout="responsive"
-        //                 />
-        //             </div>
-        //         </div>
-        //     : null }
-        // </>
+        
+        <>
+            { loading ? 
+                <div className="loader">
+                    <div className="loader__logo text-center">
+                        <Image src="/images/logo_find-persons.png" alt="Logo" width={100} height={100} 
+                            layout="responsive"
+                            style={{ width: '400px', height: '400px' }}
+                        />
+                    </div>
+                </div>
+            : null }
+            
+        </>
+
     )
     
 }
