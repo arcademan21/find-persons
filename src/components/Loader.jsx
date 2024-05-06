adb27f7094aa13e3b65571daf6212f45aecd858e
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-06 18:37:09 +0200

    Subiendo cambios del front

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -25,7 +25,7 @@ const Loader = () => {
             { loading ? 
                 <div className="loader">
                     <div className="loader__logo text-center">
-                        <Image src="/images/logo_find-persons.png" alt="Logo" width={200} height={100} />
+                        <Image src="/images/logo_find-persons.png" alt="Logo" width={200} height={200} />
                     </div>
                 </div>
             : null }
