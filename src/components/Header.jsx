14530290f0d3e4bd1275596ea13214a35190f5dd
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-08 16:34:27 +0200

    Subiendo cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,8 +23,8 @@ const Header = () => {
       <nav className="navbar navbar-expand-lg navbar-light navbar-float p-0">
         <div className="d-flex w-100 justify-content-between px-2">
           <div className="d-flex content-link-logo">
-            {pathname}
-            {pathname !== 'payment' ?
+            
+            {pathname !== '/payment' ?
               <a href={extension} className="navbar-brand link-logo ">
                 <Image src="/images/logo_find-persons.png" width={150} height={60} alt="logo" />
               </a>
