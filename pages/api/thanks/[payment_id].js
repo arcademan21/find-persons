3e2b726c632193636bc261ea108e3524f0d351e8
ArcadeManXXI <54552431+arcademan21@users.noreply.github.com>
2024-05-23 14:02:58 +0200

    Añadiendo los correos de fr de it

--- pages/api/thanks/[payment_id].js
+++ pages/api/thanks/[payment_id].js
@@ -195,9 +195,8 @@ export default function handler( req, res ) {
 
     ExistsPayment( payment_id )
     .then(paymentExists => {
-        throw new Error(JSON.stringify(paymentExists))
-        //if (!paymentExists) throw new Error('payment_not_found')
-        //return CheckTokenValidity(payment_token)
+        if (paymentExists.status === 'error') throw new Error('payment_already_exists')
+        return CheckTokenValidity(payment_token)
     })
     .then(tokenIsValid => {
         if (!tokenIsValid) throw new Error('invalid_token')
