d2f856ffd998e2380e7a3877f612b6f1dacb8418
Premiumadsselected <administracion@adsselected.com>
2024-05-30 14:46:37 +0200

    Version que marca converciones

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,7 +7,6 @@ import Footer from "../src/components/Footer"
 import Script from 'next/script'
 import Head from 'next/head'
 import 'react-toastify/dist/ReactToastify.css'
-//import {useEffect, useState} from 'react'
 
 export const metadata = {
   title: 'Find - Persons',
@@ -19,28 +18,6 @@ export const metadata = {
 
 function PersonsFinder({ Component, pageProps }) {
 
-  //const [analiticsTag, setAnaliticsTag] = useState(null)
-
-  // useEffect(() => {
-  //   const extension = localStorage.getItem('extencion')
-    
-  //   if( extension === null || extension === undefined || extension === '/') 
-  //     setAnaliticsTag(process.env.NEXT_PUBLIC_ANALYTICS_TAG_ES)
-    
-  //   else if( extension === '/es' )
-  //     setAnaliticsTag(process.env.NEXT_PUBLIC_ANALYTICS_TAG_ES)
-
-  //   else if( extension === '/it' ) 
-  //     setAnaliticsTag(process.env.NEXT_PUBLIC_ANALYTICS_TAG_IT)
-    
-  //   else if( extension === '/fr' ) 
-  //     setAnaliticsTag(process.env.NEXT_PUBLIC_ANALYTICS_TAG_FR)
-
-  //   else if( extension === '/de' )
-  //     setAnaliticsTag(process.env.NEXT_PUBLIC_ANALYTICS_TAG_DE)
-    
-  // }, [])
-  
   return (<>
     
     <Head>
@@ -65,19 +42,7 @@ function PersonsFinder({ Component, pageProps }) {
           `
         }} />
 
-        {/* Google tag (gtag.js) ANALYTICS */}
-        {/* <script 
-        async 
-        src={`https://www.googletagmanager.com/gtag/js?id=${analiticsTag}`}
-        ></script>
-        <script dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', ${analiticsTag});
-          `
-        }} /> */}
+        
 
     </Head>
 
@@ -107,20 +72,7 @@ function PersonsFinder({ Component, pageProps }) {
     <Script type="text/javascript" src="/js/theme.js" strategy='lazyOnload' />
     <Script type="text/javascript" src="/libs/wow/wow.min.js" strategy="beforeInteractive" />
     <Script type="text/javascript" src="/libs/wow/wow.init.js" strategy="afterInteractive" />
-
-    {/* <script dangerouslySetInnerHTML={{
-      __html: `
-      window._mfq = window._mfq || [];
-      (function() {
-          var mf = document.createElement("script");
-          mf.type = "text/javascript"; mf.defer = true;
-          mf.src = "//cdn.mouseflow.com/projects/b954db58-6476-4e67-8b81-d66b3eaa740f.js";
-          document.getElementsByTagName("head")[0].appendChild(mf);
-      })();
-      `
-    }} /> */}
     
-
   </>)
 
 }
