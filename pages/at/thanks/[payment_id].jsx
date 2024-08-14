ab08109945cb9b1e0816f9b2ef5a41e52f7e0b43
NothingTowinDev <nothingtowindev@gmail.com>
2024-08-14 16:51:05 +0200

    add -- etiquetas de conversion y analitics para be y at

--- pages/at/thanks/[payment_id].jsx
+++ pages/at/thanks/[payment_id].jsx
@@ -5,7 +5,7 @@ import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 const path_endpoint = process.env.NEXT_PUBLIC_PATH_END_POINT
-const convertions_gtag = process.env.NEXT_PUBLIC_CONVERTIONS_GTAG
+const convertions_gtag = process.env.NEXT_PUBLIC_CONVERTIONS_GTAG_AT
 
 export const CheckTokenValidity = async ( token ) => {
     
