227d11a654ce0e180441d2546720488c6be0461b
arcademan21 <haroldyarturo@gmail.com>
2024-08-29 12:07:52 +0200

    add -- script cookieyes en /ae

--- pages/ae/index.jsx
+++ pages/ae/index.jsx
@@ -3,11 +3,13 @@ import HomeSections from '@/components/HomeSections'
 import styles from "@/app/css/page.module.css"
 
 const Home = () => {
-    
-    return (<>
-        <Hero />
-        <HomeSections />
-    </>)
+
+  return (<>
+    <Hero />
+    <HomeSections />
+    <script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/351f4de424a0d0b99de31a9b/script.js" async></script>
+
+  </>)
 
 }
 
