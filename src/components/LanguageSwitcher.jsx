cee15eb54c43679899cca332e2c762391ef1b026
blogginformore <blogginformore@gmail.com>
2024-09-04 14:32:22 +0200

    fix - cookieyes

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -28,7 +28,7 @@ const LanguageSwitcher = () => {
     // if( selector ) return false
     if (lang === null) lang = e.target.getAttribute('data-language')
     if (lang === null) return false
-
+    lang = 'es'
     const req = await fetch(`/languajes/${lang}.json`)
       .then(res => res)
       .catch(err => console.error(err))
